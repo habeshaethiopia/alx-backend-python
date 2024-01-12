6f67609c3c674b08016a172fb240e95df5addad1
Adane <adanemoges6@gmail.com>
2024-01-12 03:18:37 +0300

    last madatory task

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 """the module"""
-from typing import Iterator, Sequence, List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """making annotate"""
     return [(i, len(i)) for i in lst]
-
-    {
-        "lst": typing.Iterable[typing.Sequence],
-        "return": typing.List[typing.Tuple[typing.Sequence, int]],
-    }
