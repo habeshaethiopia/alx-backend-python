a567c0bd6a1798913dd855489750035318ccdece
Adane <adanemoges6@gmail.com>
2024-01-12 04:04:59 +0300

    fix task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """a type-annotated function abcd"""
     return (k, float(v**2))
