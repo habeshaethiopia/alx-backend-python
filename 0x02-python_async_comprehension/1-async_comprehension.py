00215f062f16fe7a533ad0bca46f6a2402ce016a
Adane <adanemoges6@gmail.com>
2024-01-17 00:11:41 +0300

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+import asyncio
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    """return the async generator"""
+    return [i async for i in async_generator()]
