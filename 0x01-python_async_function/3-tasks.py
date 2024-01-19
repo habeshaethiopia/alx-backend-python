c975ff9716de85b748412b17023bdbf05e4627de
Adane <adanemoges6@gmail.com>
2024-01-19 09:35:16 +0300

    task 3 done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Create a measure_time function with integers n and max_delay
+as arguments that measures the total execution time"""
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+import asyncio
+
+
+def task_wait_random(max_delay):
+    """function to create asycoio task"""
+    return asyncio.create_task(wait_random(max_delay))
