1f3e39e94e09ffcb2cd0b78baa406252a6fc4812
Adane <adanemoges6@gmail.com>
2024-01-19 09:43:54 +0300

    task 3,4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+Write an asynchronous coroutine that takes
+in an integer argument (max_delay, with a default
+value of 10) named wait_random that waits for a random
+delay between 0 and max_delay (included and float value)
+seconds and eventually returns int
+"""
+import asyncio
+import typing
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """function that wait random delay"""
+    return sorted([await task_wait_random(max_delay) for _ in range(n)])
