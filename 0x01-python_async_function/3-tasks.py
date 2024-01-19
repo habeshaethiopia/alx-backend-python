82ff1986a07cd0acb5749b53c593d8e0182f7419
Adane <adanemoges6@gmail.com>
2024-01-19 10:31:38 +0300

    variable annotation is fixed for task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,8 +2,8 @@
 """Create a measure_time function with integers n and max_delay
 as arguments that measures the total execution time"""
 
-wait_random = __import__("0-basic_async_syntax").wait_random
 import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
