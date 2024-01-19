4f8c36eac7664fe9f6a76d72271a03afc6c13c73
Adane <adanemoges6@gmail.com>
2024-01-19 09:12:54 +0300

    doc check for task1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Write an asynchronous coroutine that takes
+"""
+Write an asynchronous coroutine that takes
 in an integer argument (max_delay, with a default
 value of 10) named wait_random that waits for a random
 delay between 0 and max_delay (included and float value)
-seconds and eventually returns it."""
+seconds and eventually returns int
+"""
 import asyncio
 import typing
 
@@ -11,5 +13,5 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
-    """pass"""
+    """function that wait random delay"""
     return sorted([await wait_random(max_delay) for _ in range(n)])
