bdbdfcb6e90f770dd369937792ee784d8e1a11f1
Adane <adanemoges6@gmail.com>
2024-02-06 00:00:00 +0300

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,13 @@ class TestAccessNestedMap(TestCase):
     ):
         """Test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    def test_access_nested_map_exception(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        exception: Exception,
+    ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
